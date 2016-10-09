cc4f3d7fa73e93776ea7fe74fcbe8b56c1ba107d
Drumil Mahajan <dm3804@nyu.edu>
2016-10-09 15:07:24 -0400

    Trying quick sort 3: Code buggy

--- Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/sorting/sorting.cpp
+++ Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/sorting/sorting.cpp
@@ -6,6 +6,7 @@ using std::vector;
 using std::swap;
 
 int partition2(vector<int> &a, int l, int r) {
+   
   int x = a[l];
   int j = l;
   for (int i = l + 1; i <= r; i++) {
@@ -18,17 +19,39 @@ int partition2(vector<int> &a, int l, int r) {
   return j;
 }
 
+std::vector<int> partition3(vector<int> &a, int l, int r) {
+  std::vector<int> m3;
+  int x = a[l];
+  int j = l;
+  int equal = 0;
+  for (int i = l + 1; i <= r; i++) {
+    if (a[i] == x) {
+      j++;
+      equal ++;
+      swap(a[i], a[j]);
+    }
+    else if (a[i] < x) {
+      j++; 
+      swap(a[i], a[j]);
+      swap(a[l], a [j]);
+      l++;
+	}
+  }
+  return {l,l+equal};
+}
+
 void randomized_quick_sort(vector<int> &a, int l, int r) {
+  std::vector<int> m;
   if (l >= r) {
     return;
   }
-
+  
   int k = l + rand() % (r - l + 1);
   swap(a[l], a[k]);
-  int m = partition2(a, l, r);
-
-  randomized_quick_sort(a, l, m - 1);
-  randomized_quick_sort(a, m + 1, r);
+  m = partition3(a, l, r);
+  std::cout<<a[m[0]]<<" "<<a[m[1]];
+  //randomized_quick_sort(a, l, m[0] - 1);
+  //randomized_quick_sort(a, m[1] + 1, r);
 }
 
 int main() {
@@ -39,7 +62,7 @@ int main() {
     std::cin >> a[i];
   }
   randomized_quick_sort(a, 0, a.size() - 1);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cout << a[i] << ' ';
-  }
+  //for (size_t i = 0; i < a.size(); ++i) {
+  //  std::cout << a[i] << ' ';
+  //}
 }
