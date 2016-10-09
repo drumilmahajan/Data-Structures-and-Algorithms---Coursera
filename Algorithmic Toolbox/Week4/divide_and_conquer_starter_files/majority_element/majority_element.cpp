2bb30f56eb6781c41e33da488c9894738d5cd597
Drumil Mahajan <dm3804@nyu.edu>
2016-10-09 01:14:56 -0400

    Recursion calls added: one bug

--- Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/majority_element/majority_element.cpp
+++ Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/majority_element/majority_element.cpp
@@ -5,21 +5,42 @@
 using std::vector;
 
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  if ()
+  if (right == -1) return -1;
+  if (left == right) return a[left];
 
   else {
 
-    int major_l = get_majority_element(a , left , right/2);
-    int major_r = get_majority_element(a, right/2+1 , right);
-
-    if()
-
+    int major_l = get_majority_element(a , left , ((right - left ) / 2) + left);
+    int major_r = get_majority_element(a, ((right-left)/2) + 1 + left , right);
+    
+    if ( major_r == -1 && major_l <  == -1 ) return -1;
+    
+    if (major_l == major_r)
+    {
+    	return major_l;
+	}
+	
+	if (major_l >=0) {
+		for(int i = ((right-left)/2) + 1; i <= right ; i++){
+			if(major_l == a[i]) {
+				return major_l;
+			}
+		}
+		return -1;	
+	}
+	
+	if ( major_r >= 0){
+		for(int i = left ; i <= (right-left)/2 ; i++) {
+			if(major_r == a[i]) {
+				return major_r;
+			}
+		}
+		return -1;
+	}
+	
   }
-
-  return -1;
 }
+
 
 int main() {
   int n;
@@ -28,5 +49,5 @@ int main() {
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element(a, 0, a.size()-1)) << '\n';
 }
