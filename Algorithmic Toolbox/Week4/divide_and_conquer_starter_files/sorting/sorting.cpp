f55f4b68610d3fdb4d4c4ab6ecd779b8a102c2d4
Drumil Mahajan <dm3804@nyu.edu>
2016-10-10 16:50:17 -0400

    Quick sort three successfully implemented

--- Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/sorting/sorting.cpp
+++ Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/sorting/sorting.cpp
@@ -20,7 +20,7 @@ int partition2(vector<int> &a, int l, int r) {
 }
 
 std::vector<int> partition3(vector<int> &a, int l, int r) {
-  std::vector<int> m3;
+  std::vector<int> m3(2);
   int x = a[l];
   int j = l;
   int equal = 0;
@@ -36,8 +36,13 @@ std::vector<int> partition3(vector<int> &a, int l, int r) {
       swap(a[l], a [j]);
       l++;
 	}
+  //std::cout<<"l :"<<l<<"\n";
+  //std::cout<<"l+equal :"<<l+equal<<"\n";
   }
-  return {l,l+equal};
+  m3[0] = l;
+  m3[1] = l+equal;
+  //std::cout<<"\n";
+  return m3;
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
@@ -47,11 +52,15 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
   }
   
   int k = l + rand() % (r - l + 1);
+  
   swap(a[l], a[k]);
+  //std::cout<<"k : "<<k<<"\n";
+  
+  //std::cout<<"\n";
   m = partition3(a, l, r);
-  std::cout<<a[m[0]]<<" "<<a[m[1]];
-  //randomized_quick_sort(a, l, m[0] - 1);
-  //randomized_quick_sort(a, m[1] + 1, r);
+  //std::cout<<m[0]<<" "<<m[1];
+  randomized_quick_sort(a, l, m[0] - 1);
+  randomized_quick_sort(a, m[1] + 1, r);
 }
 
 int main() {
@@ -62,7 +71,7 @@ int main() {
     std::cin >> a[i];
   }
   randomized_quick_sort(a, 0, a.size() - 1);
-  //for (size_t i = 0; i < a.size(); ++i) {
-  //  std::cout << a[i] << ' ';
-  //}
+  for (size_t i = 0; i < a.size(); ++i) {
+    std::cout << a[i] << ' ';
+  }
 }
