fe14ae011f28cf279a7b18b0f81096889f70a8ed
Drumil Mahajan <dm3804@nyu.edu>
2016-10-14 23:53:17 -0400

    Arbitrart tree height calculator added: Potential bugs

--- Data Structures/Week1/Starters PA1/tree_height/tree-height.cpp
+++ Data Structures/Week1/Starters PA1/tree_height/tree-height.cpp
@@ -5,26 +5,48 @@
 class TreeHeight {
   int n;             
   std::vector<int> parent;
+  int h;
+  std::vector<int> height;
 
 public:
+		
   void read() {
     std::cin >> n;
     parent.resize(n);
-    for (int i = 0; i < n; i++)
+    height.resize(n);
+	for (int i = 0; i < n; i++){
+	
       std::cin >> parent[i];
+      height[i] = -1;
+  	}
   }
+  	
+	int compute_height() {
+	
+	for( int i = 0 ; i < n ; i++) {
+		if (height[i] == -1){
+			height[i] = compute_height_of_node(parent[i]);
+			std::cout<<"height : "<<height[i]<<std::endl;
+			h = std::max(h, height[i]);
+			}	
+		}
+	return h;
+	}
 
-  int compute_height() {
-    // Replace this code with a faster implementation
-    int maxHeight = 0;
-    for (int vertex = 0; vertex < n; vertex++) {
-      int height = 0;
-      for (int i = vertex; i != -1; i = parent[i])
-        height++;
-      maxHeight = std::max(maxHeight, height);
-    }
-    return maxHeight;
-  }
+	int compute_height_of_node(int node) { 
+	//std::cout<<"I am the recursive function and I am called "<<std::endl ;
+    if ( node == -1) {
+    	return 1;
+	}
+	else {
+		if (height[node] == -1) {
+			height[node] = compute_height_of_node(parent[node]); 	
+			return 1 + height[node];
+		}
+		else
+			return 1 + height[node];
+		}
+  	}
 };
 
 int main() {
