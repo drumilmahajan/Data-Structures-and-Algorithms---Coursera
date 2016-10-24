6f1fb6f42ad9a0bce67205abfa239e0238dbdce2
Drumil Mahajan <dm3804@nyu.edu>
2016-10-23 23:11:44 -0400

    Quick sort-3 Code commented for ease of reading

--- Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/sorting/sorting.cpp
+++ Algorithmic Toolbox/Week4/divide_and_conquer_starter_files/sorting/sorting.cpp
@@ -4,42 +4,31 @@
 
 using std::vector;
 using std::swap;
-// This function is provided by coursera which uses two partitioning in quick sort. Bad for equal elements. 
-int partition2(vector<int> &a, int l, int r) {
-   
-  int x = a[l];
-  int j = l;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] <= x) {
-      j++;
-      swap(a[i], a[j]);
-    }
-  }
-  swap(a[l], a[j]);
-  return j;
-}
+
 
 // Three way partitioning created to take into account equal elements. 
 std::vector<int> partition3(vector<int> &a, int l, int r) {
   std::vector<int> m3(2); // Vecotr to store left and right indicies of equal elements. 
-  int x = a[l];
-  int j = l;
-  int equal = 0;
-  for (int i = l + 1; i <= r; i++) {
-    if (a[i] == x) {
-      j++;
-      equal ++;
-      swap(a[i], a[j]);
+  int x = a[l]; // storing the pivot as the first element
+  int j = l; // j is the marker for the elemnts less than or equal to the pivot.
+  int equal = 0; // stores the number of elements equal to pivot found (excluding the pivot) 
+  for (int i = l + 1; i <= r; i++) {  // Traversing the array from the element next to pivot to the last. 
+    if (a[i] == x) { // In case of equal element. 
+      j++; // Moving the j pointer
+      equal ++; // Incrementing the number of equal elemnts by 1. 
+      swap(a[i], a[j]); // swapping i and j. 
     }
+    // If a[x] < x 
     else if (a[i] < x) {
-      j++; 
-      swap(a[i], a[j]);
-      swap(a[l], a [j]);
-      l++;
+      j++; // MOving the marker 
+      swap(a[i], a[j]); // Swapping to move the smaller element to the left side 
+      swap(a[l], a [j]); // Swapping to keep the equal elements together
+      l++; // Incrementing l to keep all equal elements together. 
 	}
   //std::cout<<"l :"<<l<<"\n";
   //std::cout<<"l+equal :"<<l+equal<<"\n";
   }
+  // returning the two pivots where all elements between l and l+equal are equal
   m3[0] = l;
   m3[1] = l+equal;
   //std::cout<<"\n";
@@ -47,6 +36,7 @@ std::vector<int> partition3(vector<int> &a, int l, int r) {
 }
 
 void randomized_quick_sort(vector<int> &a, int l, int r) {
+  
   std::vector<int> m;
   if (l >= r) {
     return;
@@ -56,13 +46,13 @@ void randomized_quick_sort(vector<int> &a, int l, int r) {
   
   swap(a[l], a[k]);
   m = partition3(a, l, r); // Receives the two indicies from partition three procedure and make recursive calls. 
-  randomized_quick_sort(a, l, m[0] - 1);
-  randomized_quick_sort(a, m[1] + 1, r);
+  randomized_quick_sort(a, l, m[0] - 1);  // recursive call to left half
+  randomized_quick_sort(a, m[1] + 1, r); // recursive call to right half
 }
 
 int main() {
-  int n;
-  std::cin >> n;
+  int n; // number of elements
+  std::cin >> n; 
   vector<int> a(n);
   for (size_t i = 0; i < a.size(); ++i) {
     std::cin >> a[i];
