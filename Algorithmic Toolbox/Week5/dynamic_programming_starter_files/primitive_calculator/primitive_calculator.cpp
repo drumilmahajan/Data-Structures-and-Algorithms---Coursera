2dec67816ad96adc267a622094b8aab793622268
Drumil Mahajan <dm3804@nyu.edu>
2016-11-12 22:27:16 -0500

    dynamic programming algo implemented: minor bugs

--- Algorithmic Toolbox/Week5/dynamic_programming_starter_files/primitive_calculator/primitive_calculator.cpp
+++ Algorithmic Toolbox/Week5/dynamic_programming_starter_files/primitive_calculator/primitive_calculator.cpp
@@ -1,50 +1,81 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using std::vector;
 
-struct Node {
-	int key_value; // value of the node
-	Node *left; // left node will contain result of num -1 operation
-	Node *middle; // middle node will contain result of /2 operation if divisible
-	Node *right; // right node will contain result of /3 operation if divisible
-};
+std::map <int, int> recursion_values;
 
+int optimal_sequence_dp(int n){
 
-vector<int> optimum_sequence_dp(int n){
-	std::vector<int> sequence;
-	Node root = new Node;
-	node->
-	
-	while(n >= 1) {
+	// Reccursion base cases
+	if(n == 0)
+		return 0;
+	else if ( n == 1 )
+		return 0;
+	else if ( n == 2 || n == 3) 
+		return 1;
+
+	else {
 		
-	}
+		int n_3_op = 2147483647; 
+		int n_2_op = 2147483647; 
+		int n_1_op = 2147483647; 
+		
+		if (n%3 == 0) {
+			int n_3_q = n/3;
+			
+			if (recursion_values.find(n_3_q) == recursion_values.end()) {
+				n_3_op = optimal_sequence_dp(n_3_q);
+				recursion_values.insert({n_3_q, n_3_op});
+			}
+			else {
+				n_3_op = recursion_values.find(n_3_q)->second;	
+			}
+		}
+		
+		if (n%2 == 0) {
+			int n_2_q = n/2;
+			
+			if (recursion_values.find(n_2_q) == recursion_values.end()) {
+				n_2_op = optimal_sequence_dp(n_2_q);
+				recursion_values.insert({n_2_q, n_2_op});
+			}
+			else {
+				n_2_op = recursion_values.find(n_2_q)->second;	
+			}
+		}
+
+		int n_1_q = n-1;
+		if (recursion_values.find(n_1_q) == recursion_values.end()) {
+				n_1_op = optimal_sequence_dp(n_1_q);
+				recursion_values.insert({n_1_q, n_1_op});
+		}
+		else {
+			n_1_op = recursion_values.find(n_1_q)->second;	
+		}
+		
+		int minimum = std::min(n_3_op,std::min(n_2_op,n_1_op));
+		
+		//std::cout<<"else called"<<std::endl;
+		return 1 + minimum; 
+		
+	}	 	
 	
 }
 
-vector<int> optimal_sequence(int n) {
-  std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
-    }
-  }
-  reverse(sequence.begin(), sequence.end());
-  return sequence;
-}
 
 int main() {
   int n;
   std::cin >> n;
-  vector<int> sequence = optimal_sequence(n);
-  std::cout << sequence.size() - 1 << std::endl;
+  //vector<int> sequence = optimal_sequence(n);
+  int sequence = optimal_sequence_dp(n);	
+  //std::cout << sequence.size() - 1 << std::endl;
+  /*
   for (size_t i = 0; i < sequence.size(); ++i) {
     std::cout << sequence[i] << " ";
   }
+  */
+  std::cout << sequence << " ";
 }
