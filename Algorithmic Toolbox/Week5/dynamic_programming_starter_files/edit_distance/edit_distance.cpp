ce2fbcfc0b007bd7d566eb5cabf7b37d991ff7af
Drumil Mahajan <dm3804@nyu.edu>
2016-11-14 00:15:12 -0500

    edit distance algorithm successfully implemented

--- Algorithmic Toolbox/Week5/dynamic_programming_starter_files/edit_distance/edit_distance.cpp
+++ Algorithmic Toolbox/Week5/dynamic_programming_starter_files/edit_distance/edit_distance.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using std::string;
 
 int edit_distance(const string &str1, const string &str2) {
-  //write your code here
-  return 0;
+
+  std::vector< std::vector<int> > D;
+  // resizing for rows and columns
+  // D.resize(rows , vector<Type>(columns, initial value))
+  D.resize(str1.length() + 1, std::vector<int>(str2.length() + 1 , 0));
+
+  for ( int j = 1 ; j <= str2.length() ; j++) {
+    for ( int i = 1 ; i <= str1.length() ; i++) {
+      int insertion = D[i][j-1] + 1;
+      int deletion = D[i-1][j] + 1;
+      int match = D[i-1][j-1];
+      int mis_match = D[i-1][j-1] + 1;
+      if ( str1[i] == str2[j])
+        D[i][j] = std::min(insertion , std::min(deletion, match));
+      else
+        D[i][j] = std::min(insertion , std::min(deletion, mis_match));
+
+    }
+  }
+
+  return D[str1.length()][str2.length()];
 }
 
 int main() {
