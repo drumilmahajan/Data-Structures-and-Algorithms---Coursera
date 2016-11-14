1f1e9decf03f090bf7ca7c8cd77836b2ae1aa0ef
Drumil Mahajan <dm3804@nyu.edu>
2016-11-13 21:47:15 -0500

    knapsack without repetations solved

--- Algorithmic Toolbox/Week5/dynamic_programming_starter_files/knapsack/knapsack.cpp
+++ Algorithmic Toolbox/Week5/dynamic_programming_starter_files/knapsack/knapsack.cpp
@@ -4,15 +4,37 @@
 using std::vector;
 
 int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+
+  // initializing first row and first column to zero
+
+  //int value[W + 1][w.size()+1];
+  vector<vector<int> > value;
+  value.resize(W + 1, vector<int>(w.size() + 1 , 0));
+
+  for( int i = 0; i < W + 1; i++) {
+        value[i][0] = 0;
+  }
+
+  for( int j = 0; j < w.size() + 1; j++){
+        value[0][j] = 0;
+  }
+
+  for( int i = 1; i < w.size() + 1; i++) {
+    for( int j = 1; j < W + 1; j++){
+        value[j][i] = value[j][i-1];
+        //std::cout<<value[i][j]<<std::endl;
+        if( w[i-1] <= j){
+            int val = value[j - w[i-1]][i-1] + w[i-1];
+            //std::cout<<val<<std::endl;
+            if ( value[j][i] < val)
+                value[j][i] = val;
+        }
     }
   }
-  return current_weight;
+  return value[W][w.size()];
 }
+
+
 
 int main() {
   int n, W;
